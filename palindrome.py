14cadedbff4d3146cf57f052089a01e8fc74e43d
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-09-19 13:53:53 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,22 @@
-str1=input("Enter a string: ")
+# str1=input("Enter a string: ")
 
-def is_palindrome(s):
-    if s == s[::-1]:
-        print("The string is a palindrome.")
-    else:
-        print("The string is not a palindrome.")
+# def is_palindrome(s):
+#     if s == s[::-1]:
+#         print("The string is a palindrome.")
+#     else:
+#         print("The string is not a palindrome.")
 
-is_palindrome(str1)
+# is_palindrome(str1)
 
+givenstr=input("Enter a string: ")
+
+isPalindrome = True
+
+for i in range(len(givenstr)//2):
+    if givenstr[i] != givenstr[len(givenstr)-1-i]:
+        isPalindrome = False
+        break
+if isPalindrome:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")

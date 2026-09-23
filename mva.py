e1c22c1a3de8b7e39adeaf88a6c47d424ca67f2e
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-09-23 22:01:08 +0530

    Update mva.py

--- mva.py
+++ mva.py
@@ -2,11 +2,29 @@ nums = list(map(int, input().split()))
 n=len(nums)
 
 
-def mvalgo(nums):
-        n = len(nums)
-        nums.sort()
+# def mvalgo(nums):
+#         n = len(nums)
+#         nums.sort()
 
-        return nums[n // 2]
+#         return nums[n // 2]
+
+def mvalgo(nums):
+    nums.sort()
+    majority_ele=nums[0]
+    max_count=0
+    count=1
+
+    for i in range(n-1):
+        if nums[i]!=nums[i+1]:
+            if max_count<count:
+                majority_ele=nums[i]
+                max_count=count
+            count=1
+        else:
+            count+=1        
+            
+
+    return majority_ele,max_count  
 
 
 print(mvalgo(nums))
